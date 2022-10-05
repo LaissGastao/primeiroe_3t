895badbde2464db44af869c0628d3a13aaf4b659
LaissGastao <110147241+LaissGastao@users.noreply.github.com>
2022-10-05 09:08:04 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,30 @@
+let tabuada = 3;
+
+function escreva(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+function minha Tabuada(){
+    for(let i=1; i <= 10; i++){
+        for(let j= 1; <= 10; j++){
+      document.write(" o valor de i é " + i + " <br)
+    }
+    document.write(i + "x" + j + " = " (j*i)+"<br>");
+  }
+  document.write("<br>");
+}
+
+function quadrado(){
+    for(let i = 1; i < 10; i++){
+        document.write("o quadrado de" + i + "é" + (i*i) + "<br>)"
 
