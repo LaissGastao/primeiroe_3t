ae053c25271c05d6cdfe16496a1de5189c0c5af8
LaissGastao <110147241+LaissGastao@users.noreply.github.com>
2022-10-06 09:07:27 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -28,3 +28,18 @@ function quadrado(){
     for(let i = 1; i < 10; i++){
         document.write("o quadrado de" + i + "é" + (i*i) + "<br>)"
 
+
+
+        function calcula(){
+          let val = document.getElementById("valor").value;
+          let j = document.getElementById("juros").value;
+          let t = document.getElementById("meses").value;
+
+            let res = val * (1+(j/100))
+            document.write("Resultado: +res");
+        }
+
+        
+
+
+
